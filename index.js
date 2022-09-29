4ec930867e3e1a240bd5d23cdf3d9bce3dabe833
apietryga <antek.pietryga@gmail.com>
2022-09-29 17:23:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+const apps = fs.readdirSync( __dirname + "/apps" )
+
+let content = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <main>
+    <h1> Apps </h1>
+`
+apps.forEach( app => {
+  content += `
+    <h2> ${ app } </h2>
+  `
+
+
+})
+
+// console.log(apps)
+// content += `HELLO WORLD`
+
+
+
+content += `
+  </main>
+</body>
+</html>`
+
+fs.writeFileSync( __dirname + "/index.html", content)
+
+
+
+
+
+
+
+
+
+// var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('path/to/templates'));
+// env.render('layout-foo.html', { bar: baz });
