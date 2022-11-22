db84a1cbc085f5ff8359b1095981a88938f26232
apietryga <antek.pietryga@gmail.com>
2022-11-22 20:02:10 +0100

    JS comments

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+// stałą rArr przypisujemy do klasy .right pobranej z dokumentu HTML (stała rArr jest buttonem .right)
 const rArr = document.querySelector('.right')
+// do pobranego buttona przyczepiamy słuchacza wydarzeń, który pobiera 2 argumenty:
+// 1. Rodzaj wydarzenia "click" ( na co słuchacz ma czekać )
+// 2. Funkcja - co ma się dziać, jeżeli wydarzenie się wydarzy
 rArr.addEventListener('click', function () {
+  // ten fragment wykona się dopiero po kliknięciu na prawą strzałkę
+
+  // pobieramy aktywny element z HTMLA ( ten z klasą 'active' )
   const activeElement = document.querySelector('.active');
+
+  // usuwamy z aktywnego elementu klasę 'active'
   activeElement.classList.remove('active')
 
+  // pobieramy element który jako kolejny będzie aktywny (kolejny z rodzeństwa)
   let nextActiveElement = activeElement.nextElementSibling
+
+  // jeżeli nie ma kolejnego rodzeństwa
   if( !nextActiveElement ){
+    // to do zmiennej nextActiveElement przypisz pierwszy element z rodzeństwa
     nextActiveElement = document.querySelector('.carousel-inner').firstElementChild
   }
+
+  // do kolejnego aktywnego elementu przypisz klasę active
   nextActiveElement.classList.add('active')
 
 })
-
-
-
